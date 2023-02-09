a51c35831438513da41d7fe27ddad7e68ef04d51
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-02-09 12:41:07 +0100

    I've created the components Light and Dark

---
+++ src/styles/Dark.ts
@@ -0,0 +1,10 @@
+export const dark = {
+  title: "dark",
+
+  colors: {
+    backgroundColor: "#000",
+    text: "#fff",
+    primary: "#a433",
+    second: "#f212",
+  },
+};
---
+++ src/styles/Light.ts
@@ -0,0 +1,10 @@
+export const light = {
+  title: "light",
+
+  colors: {
+    backgroundColor: "#fff",
+    text: "#000",
+    primary: "#a433",
+    second: "#f212",
+  },
+};
