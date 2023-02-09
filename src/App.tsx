6a3f6c48d52c26650d413f62d2a1c0581f828d6d
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-02-09 11:37:56 +0100

    I've created the components Home and MyStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { GlobalStyle } from "./styles/MyStyles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+
 function App() {
-  return <div className="App">Pascoal</div>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
