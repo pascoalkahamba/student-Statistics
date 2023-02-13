94960b991e5d04fa6b84a604939570320058a0a3
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-02-13 10:27:18 +0100

    I'm cleaning some code

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,18 +3,12 @@ import { Switch } from "@mui/material";
 import { ThemeContext } from "styled-components";
 import { Header } from "../styles/MyStyles";
 
-import useGlobalStarage from "../hooks/useGlobalStarage";
-
 interface HeadProps {
   toggleTheme: () => void;
 }
 const Head = ({ toggleTheme }: HeadProps) => {
   const { title } = useContext(ThemeContext);
-  const {
-    global: { name },
-  } = useGlobalStarage();
 
-  console.log(name);
   return (
     <Header>
       <h3>Pascoal Kahamba</h3>
