3cf6ef71607eceb70f4b47a2fd50620257d2e4a6
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-02-20 20:26:12 +0100

    Improving the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useTheme } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import { GlobalStorage } from "./components/GlobalContext";
 import Head from "./components/Head";
 import Home from "./components/Home";
-import { GlobalStyles, Header } from "./themes/MyStyles";
 import PageNumberDisciplines from "./components/PageNumberDisciplines";
 
 function App() {
