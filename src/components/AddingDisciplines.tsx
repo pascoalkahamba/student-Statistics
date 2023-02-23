9181813a2346e4ae1f8a4c52fdd2c7ded708ffa6
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-02-23 13:41:32 +0100

    I'm putting content on the component AddingDiciplines

--- src/components/AddingDisciplines.tsx
+++ src/components/AddingDisciplines.tsx
@@ -1,9 +1,63 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme, TextField, Button } from "@mui/material";
+import CustomizedSnackbars from "./CustomizedSnackbars";
 
 const AddingDisciplines = () => {
+  const {
+    palette: {
+      primary: { dark },
+    },
+  } = useTheme();
+
   return (
     <Box sx={{ width: "100%", marginTop: "80px", padding: ".5rem" }}>
-      AddingDisciplines
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={() => ({
+          backgroundColor: dark,
+          textAlign: "center",
+          borderRadius: ".3rem",
+        })}
+      >
+        Disciplina
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "100" },
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "2rem",
+          flexDirection: "column",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Box
+          component="div"
+          sx={{
+            display: "flex",
+            gap: "5rem",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Nome da disciplina"
+            variant="outlined"
+          />
+          <TextField
+            type="number"
+            id="outlined-basic"
+            label="Nota da disciplina"
+            variant="outlined"
+          />
+        </Box>
+        <Button variant="contained" size="large">
+          Adicionar
+        </Button>
+      </Box>
+      <CustomizedSnackbars />
     </Box>
   );
 };
